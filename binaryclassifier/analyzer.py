e58cf850126f7f8daa0cf0528e771dc27e5561ad
bmathias12 <bmathias@speedeondata.com>
2020-03-04 16:32:38 -0500

    added roc curve to analyzer

--- binaryclassifier/analyzer.py
+++ binaryclassifier/analyzer.py
@@ -1,4 +1,5 @@
-
+from binaryclassifier.plotting import plot_roc_curve
+from binaryclassifier.plotting.colors import THEME_COLORS
 
 class Analyzer:
     
@@ -6,4 +7,9 @@ class Analyzer:
         self.y_true = y_true
         self.scores = scores
         self.preds = preds
+
+    def plot_roc_curve(self, color=THEME_COLORS[0]):
+        return plot_roc_curve(self.y_true, self.scores, color=color)
+
+
 
---
+++ binaryclassifier/plotting/__init__.py
@@ -0,0 +1,1 @@
+from binaryclassifier.plotting.roc import plot_roc_curve
