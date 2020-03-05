7a2adb8d5a20b81841dcf40883c0068ca6dc6705
bmathias12 <bmathias@speedeondata.com>
2020-03-05 11:41:21 -0500

    added kwargs to Analyzer methods

--- binaryclassifier/analyzer.py
+++ binaryclassifier/analyzer.py
@@ -16,11 +16,12 @@ class Analyzer:
         self.scores = scores
         self.preds = preds
 
-    def plot_roc_curve(self, color=THEME_COLORS[0]):
-        return plot_roc_curve(self.y_true, self.scores, color=color)
+    def plot_roc_curve(self, color=THEME_COLORS[0], **kwargs):
+        return plot_roc_curve(self.y_true, self.scores, color=color, **kwargs)
 
-    def plot_prediction_density(self, colors=THEME_COLORS):
-        return plot_prediction_density(self.y_true, self.scores, colors=colors)
+    def plot_prediction_density(self, colors=THEME_COLORS, **kwargs):
+        return plot_prediction_density(self.y_true, self.scores, colors=colors,
+            **kwargs)
 
     def plot_quantiles(self, color=THEME_COLORS[0], **kwargs):
         return plot_quantiles(self.y_true, self.scores, color=color, **kwargs)
